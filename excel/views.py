904cd007b654e9e9ca1c4690d03df51a75f9ac2a
Shri Kalanke <you@example.com>
2023-10-31 17:12:41 +0530

    multiple products

--- excel/views.py
+++ excel/views.py
@@ -101,31 +101,32 @@ def excel_view(request, sheet_name):
             current_time = current_datetime.strftime(
                 '%H:%M:%S')  # Format time as HH:mm:ss
 
-            # Append data only if the column names match and product_type is 1 or 2
+            # Append data only if the column names match and product_id is match 1 to 25
             if column_names == default_columns:
                 # ...
                 # Inside the loop that processes JSON objects
                 for obj in json_objects:
-                    product_type = obj.get('product_type', 0)
-                    if product_type not in (1, 2):
-                        return Response({"error": "Please enter a valid product_type (1 or 2)"}, status=status.HTTP_400_BAD_REQUEST)
+                    product_id = obj.get('product_id', 0)
+                    if product_id not in range(1, 29):
+                        return Response({"error": "Please enter a valid product_type (>=1 To <=28)"}, status=status.HTTP_400_BAD_REQUEST)
 
-                    if product_type == 2:  # product_type == 2 for bird and weight not needed
+                    if product_id in [4, 5, 12, 19, 22, 23, 24, 25, 26, 27, 28]:
+                        # product_id == 4 or 5 weight not needed
                         weight = obj.get('weight')
                         if weight:
                             return Response({"error": "please remove weight field..!!"}, status=status.HTTP_400_BAD_REQUEST)
                     else:
-                        # product_type == 1 for eggs and weight needed
+                        # weight needed
                         weight = obj.get('weight')
                         if not weight:
                             return Response({"error": "please enter weight..!!"}, status=status.HTTP_400_BAD_REQUEST)
 
                     # Handle the weight when appending to the sheet
                     if weight == "":
-                        row = [current_date, current_time, obj.get('shop_code', 0), product_type, obj.get('product_id', 0), "", obj.get(
+                        row = [current_date, current_time, obj.get('shop_code', 0), obj.get('product_type', 0), obj.get('product_id', 0), "", obj.get(
                             'quantity', 0.0), obj.get('daily_rate', 0.0), obj.get('rate', 0.0), ""]
                     else:
-                        row = [current_date, current_time, obj.get('shop_code', 0), product_type, obj.get('product_id', 0), weight, obj.get(
+                        row = [current_date, current_time, obj.get('shop_code', 0), obj.get('product_type', 0), obj.get('product_id', 0), weight, obj.get(
                             'quantity', 0.0), obj.get('daily_rate', 0.0), obj.get('rate', 0.0), ""]
 
                     # Append data to the sheet
@@ -142,11 +143,15 @@ def excel_view(request, sheet_name):
                 # Save the updated Excel file after appending data
                 workbook.save(file_path)
 
-                # Apply the formula after saving the sheet
+                # the formula
+                formula = '=IF(OR(E{i}=1, E{i}=2, E{i}=3, E{i}=8, E{i}=6, E{i}=7, E{i}=9, E{i}=10, E{i}=14, E{i}=13, E{i}=11, E{i}=15, E{i}=16, E{i}=17, E{i}=18, E{i}=20, E{i}=21), F{i}*H{i}, IF(OR(E{i}=4, E{i}=5, E{i}=12, E{i}=19, E{i}=22, E{i}=23, E{i}=24, E{i}=25, E{i}=26, E{i}=27, E{i}=28), G{i}*H{i}, ""))'
+
+                # Loop through the rows and apply the dynamic formula
                 for i in range(3, sheet.max_row + 1):
-                    formula = f'=IF(D{i}=1, E{i}*G{i}, IF(D{i}=2, F{i}*G{i}, ""))'
-                    sheet.cell(row=i, column=default_columns.index(
-                        "amount") + 1).value = formula
+                    # Replace {i} with the current row number
+                    row_formula = formula.format(i=i)
+                    sheet.cell(row=i, column=default_columns.index("amount") + 1).value = row_formula
+
 
                 # Save the Excel file again after applying the formula
                 workbook.save(file_path)
@@ -158,8 +163,7 @@ def excel_view(request, sheet_name):
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-
-#################################@api_view(['POST'])
+################################
 @api_view(['POST'])
 def create_daily_summary_sheet(request, sheet_name):
     if request.method == 'POST':
@@ -189,16 +193,17 @@ def create_daily_summary_sheet(request, sheet_name):
                 'paid_amount',
                 'closing_balance',
                 '',
-                'product_1',
+                'product_type',
+                'product_id',
                 'weight',
                 'quantity',
                 'rate',
                 'amount',
-                '',
-                'product_2',
-                'quantity',
-                'rate',
-                'amount',
+                '',           
+                # 'product_type',
+                # 'quantity',
+                # 'rate',
+                # 'amount',
             ]
 
             # Merge cells for the title
@@ -207,15 +212,25 @@ def create_daily_summary_sheet(request, sheet_name):
             title_cell.value = 'DAILY ACCOUNT SUMMARY'
             title_cell.alignment = Alignment(horizontal='center')
 
-            new_sheet.merge_cells(start_row=1, start_column=6, end_row=1, end_column=10)
+            #BIRDS 1 (LARGE BOILER, SMALL BOILER, GAVRAN, KADAKNATH, BATER, DUCK)
+            new_sheet.merge_cells(start_row=1, start_column=6, end_row=1, end_column=11)
             title_cell = new_sheet.cell(row=1, column=6)
-            title_cell.value = 'BIRDS'
+            title_cell.value = 'LARGE BOILER'
             title_cell.alignment = Alignment(horizontal='center')
 
-            new_sheet.merge_cells(start_row=1, start_column=12, end_row=1, end_column=15)
-            title_cell = new_sheet.cell(row=1, column=12)
-            title_cell.value = 'EGGS'
-            title_cell.alignment = Alignment(horizontal='center')
+
+            # #BIRDS 1 (LARGE BOILER, SMALL BOILER, GAVRAN, KADAKNATH, BATER, DUCK)
+            # new_sheet.merge_cells(start_row=1, start_column=6, end_row=1, end_column=10)
+            # title_cell = new_sheet.cell(row=1, column=6)
+            # title_cell.value = 'SMALL BOILER'
+            # title_cell.alignment = Alignment(horizontal='center')
+
+            #EGGS 2 (WHITE EGGS, BROWN)
+
+            # new_sheet.merge_cells(start_row=1, start_column=12, end_row=1, end_column=15)
+            # title_cell = new_sheet.cell(row=1, column=12)
+            # title_cell.value = 'EGGS'
+            # title_cell.alignment = Alignment(horizontal='center')
 
             for col_num, header in enumerate(default_columns, start=1):
                 new_sheet.cell(row=2, column=col_num, value=header)
@@ -240,7 +255,7 @@ def create_daily_summary_sheet(request, sheet_name):
             current_date = financial_year_start
             while current_date <= financial_year_end:
                 # Create a new row for each date
-                row = [current_date.strftime('%d-%m-%Y'), '', '', '', '', 1, '', '', '', '', '', 2, '', '', '']
+                row = [current_date.strftime('%d-%m-%Y'), '', '', '', '', 1, 1, '', '', '', '', '', '', '', '', '']
                 new_sheet.append(row)
 
                 # Move to the next date
@@ -248,31 +263,31 @@ def create_daily_summary_sheet(request, sheet_name):
 
             # Add the formulas to the "G" column (weight column) from $A3 to $A368
             for row in range(3, 369):
-                # For product_type 1
-                avg_weight = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1)>0,AVERAGEIFS(Raw_data_01!E:E,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1),"")'
-                new_sheet[f'G{row}'] = avg_weight
+                # For LARGE BOILER id=1
+                avg_weight = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1)>0,AVERAGEIFS(Raw_data_01!F:F,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1),"")'
+                new_sheet[f'H{row}'] = avg_weight
 
-                sum_of_quantity = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1)>0,SUMIFS(Raw_data_01!F:F,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1),"")'
-                new_sheet[f'H{row}'] = sum_of_quantity
+                sum_of_quantity = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1)>0,SUMIFS(Raw_data_01!G:G,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1),"")'
+                new_sheet[f'I{row}'] = sum_of_quantity
 
-                avg_rate = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1)>0,AVERAGEIFS(Raw_data_01!H:H,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1),"")'
-                new_sheet[f'I{row}'] = avg_rate
+                avg_rate = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1)>0,AVERAGEIFS(Raw_data_01!I:I,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1),"")'
+                new_sheet[f'J{row}'] = avg_rate
 
-                sum_of_amount = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1)>0,SUMIFS(Raw_data_01!I:I,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1),"")'
-                new_sheet[f'J{row}'] = sum_of_amount
+                sum_of_amount = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1)>0,SUMIFS(Raw_data_01!J:J,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,1),"")'
+                new_sheet[f'K{row}'] = sum_of_amount
 
-                # For product_type 2
-                sum_of_quantity = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2)>0,SUMIFS(Raw_data_01!F:F,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2),"")'
-                new_sheet[f'M{row}'] = sum_of_quantity
+                # # For product_type 2
+                # sum_of_quantity = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2)>0,SUMIFS(Raw_data_01!F:F,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2),"")'
+                # new_sheet[f'M{row}'] = sum_of_quantity
 
-                avg_rate = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2)>0,AVERAGEIFS(Raw_data_01!H:H,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2),"")'
-                new_sheet[f'N{row}'] = avg_rate
+                # avg_rate = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2)>0,AVERAGEIFS(Raw_data_01!H:H,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2),"")'
+                # new_sheet[f'N{row}'] = avg_rate
 
-                sum_of_amount = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2)>0,SUMIFS(Raw_data_01!I:I,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2),"")'
-                new_sheet[f'O{row}'] = sum_of_amount
+                # sum_of_amount = f'=IF(COUNTIFS(Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2)>0,SUMIFS(Raw_data_01!I:I,Raw_data_01!A:A,$A{row},Raw_data_01!D:D,2),"")'
+                # new_sheet[f'O{row}'] = sum_of_amount
 
-                closing_balance = f'=SUM(J{row},O{row},B{row}) - C{row}'
-                new_sheet[f'D{row}'] = closing_balance
+                # closing_balance = f'=SUM(J{row},O{row},B{row}) - C{row}'
+                # new_sheet[f'D{row}'] = closing_balance
 
             # Add the formula to the "B" column (opening_balance column) from B4 to B368
             for row in range(4, 369):
@@ -280,7 +295,7 @@ def create_daily_summary_sheet(request, sheet_name):
                 new_sheet[f'B{row}'] = formula
 
             # Format the columns
-            columns_to_format = ['G', 'H', 'I', 'J', 'M', 'N', 'O', 'B', 'C', 'D']
+            columns_to_format = ['H', 'I', 'J', 'K', 'M', 'N', 'O', 'B', 'C', 'D']
             for col_letter in columns_to_format:
                 # Format the columns to display two decimal places
                 for row in new_sheet.iter_rows(min_row=3, max_row=369, min_col=ord(col_letter) - 64, max_col=ord(col_letter) - 64):
